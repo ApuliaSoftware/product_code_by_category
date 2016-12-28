68795e17dd1dd9fd5913d017ab23d3071b989f1a
Ciro Urselli <c.urselli@apuliasoftware.it>
2016-12-28 16:18:55 +0100

    [REF] reformat code

--- models/product.py
+++ models/product.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from openerp import models, fields, api, _
+from openerp import models, fields, api, _, SUPERUSER_ID
 import re
 from openerp.exceptions import Warning as UserError
 
